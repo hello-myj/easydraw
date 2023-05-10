b2148f59832621803778536e552f0748402a3616
myj <2326629526@qq.com>
2023-05-10 16:02:37 +0800

    基本功能已经写完

--- easy_draw.h
+++ easy_draw.h
@@ -1,9 +1,13 @@
 #ifndef __MJ_EASY_DRAW_H__
 #define	__MJ_EASY_DRAW_H__
 
+#include <stdio.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdbool.h>
 
+#define ED_TEXT_DRAW_FUNC_ENABLE     1
+#define ED_TEXT_PRINTF_BUFFER        64
 
 typedef struct {
     int16_t x;
@@ -24,15 +28,25 @@ typedef struct
 
 }ed_polygonRect_helper_t;
 
+#if ED_TEXT_DRAW_FUNC_ENABLE
+
+typedef struct
+{
+    uint8_t size;
+    uint32_t bk_color;
+    uint8_t bk_color_isValid;
+}ed_text_format_config_t;
+
+#endif
 
 typedef struct
 {
   
     int      (*ed_init)(void);
     void     (*ed_pixel_draw)(int16_t x, int16_t y, uint32_t color);
-    uint32_t (*ed_pixel_get)(int16_t x, int16_t y);
+    //uint32_t (*ed_pixel_get)(int16_t x, int16_t y);
 
-
+    int     (*ed_2bit_pic)(int16_t x, int16_t y, const uint8_t* picdata, uint8_t w, uint8_t h, uint8_t isOverlay);
 
     //void     (*ed_line_draw)(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint32_t color);
     //void     (*ed_rect_draw)(int16_t x, int16_t y, uint16_t width, uint16_t height, uint32_t color);
@@ -76,20 +90,21 @@ typedef struct
 #define ED_MAX(x, y) ((x) > (y) ? (x) : (y))
 #define ED_MIN(x, y) ((x) < (y) ? (x) : (y))
 
+
+
 #ifdef __cplusplus 
 extern "C" { 
 #endif
-
-
-//int easy_draw_create_drawhandle(uint8_t buffer_len,)
 
 int  easy_draw_init(easy_draw_driver_t* easy_draw_driver, uint16_t width, uint16_t height);
 void easy_draw_set_brush_size(uint8_t size);
 
 void easy_draw_pixel(int16_t x, int16_t y, uint32_t color);
+
 void easy_draw_line(int16_t x_start, int16_t y_start, int16_t x_end, int16_t y_end, uint32_t color);
 void easy_draw_vertical_line(int16_t x, int16_t y, uint16_t h, uint32_t color);
 void easy_draw_horizon_line(int16_t x, int16_t y, uint16_t w, uint32_t color);
+
 void easy_draw_rect(int16_t x, int16_t y, uint16_t width, uint16_t height, uint32_t color);
 void easy_draw_fillRect(int16_t x, int16_t y, uint16_t width, uint16_t height, uint32_t color);
 void easy_draw_roundedrect(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t r, uint32_t color);
@@ -98,6 +113,7 @@ void easy_draw_fillRoundedRect(int16_t x, int16_t y, uint16_t w, uint16_t h, uin
 void easy_draw_arc(int xc, int yc, int r, int start_angle, int end_angle, uint32_t color);
 void easy_draw_sector(int xc, int yc, int r, int start_angle, int end_angle, uint32_t color);
 void easy_draw_fillSector(int16_t xc, int16_t yc, int16_t r, int16_t start_angle, int16_t end_angle, uint32_t color);
+
 void easy_draw_circle(int16_t x0, int16_t y0, uint16_t r, uint32_t color);
 void easy_draw_fillCircle(int16_t x0, int16_t y0, uint16_t r, uint32_t color);
 
@@ -108,11 +124,18 @@ void easy_draw_fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int1
 void easy_draw_polygon(ed_point_t* points, uint16_t num_points, uint32_t color);
 void easy_draw_fillPolygon(ed_point_t* points, uint16_t num_points, uint32_t color);
 
-//int easy_draw_bitmap(int16_t x, int16_t y, const uint8_t* bitmap, uint16_t w, uint16_t h, uint32_t color);
-int easy_draw_2bit_pic(int16_t x, int16_t y, const uint8_t* picdata, uint8_t w, uint8_t h);
+void easy_draw_2bit_pic(int16_t x, int16_t y, const uint8_t* picdata, uint8_t w, uint8_t h, uint8_t isOverlay);
+void easy_draw_4bit_pic(int16_t x, int16_t y, const uint8_t* picdata, uint8_t w, uint8_t h, uint8_t color);
 
 ed_point_t easy_helper_rotate_point(ed_point_t point, ed_point_t center, uint16_t angle_degrees);
 int easy_helper_is_polygon(ed_point_t* points, int num_points);
+
+#if ED_TEXT_DRAW_FUNC_ENABLE
+void easy_draw_set_text_format(uint8_t text_size, uint32_t bk_color, uint8_t bk_color_isValid);
+void easy_draw_text_string(int16_t x, int16_t y, char* str, uint32_t color);
+void easy_draw_text_num(int16_t x, int16_t y, uint32_t num, uint8_t len, uint32_t color);
+void easy_draw_printf(int16_t x, int16_t y, uint32_t color, const char* fmt, ...);
+#endif
 
 #ifdef __cplusplus
 }
